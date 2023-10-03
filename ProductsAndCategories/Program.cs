7b759de76abf932d68c4fbb304d5d101c51175b5
Bobby Coleman <bobbycoleman.dev@gmail.com>
2023-10-03 13:52:42 -0500

    update

--- ProductsAndCategories/Program.cs
+++ ProductsAndCategories/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.EntityFrameworkCore;
 using ProductsAndCategories.Models;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
